217f61f483340ca4d6be1efc86d453a37f5d5c02
Bruzsa László <lacika007@gmail.com>
2023-10-17 22:05:53 +0200

    new ssid, auto reconnect

--- src/WifiConnect.h
+++ src/WifiConnect.h
@@ -1,19 +1,21 @@
-const int which = 2;
+const int which = 3;
 IPAddress ip2 = IPAddress(192, 168, 1, 103);
 IPAddress ip1 = IPAddress(192, 168, 43, 247);
 IPAddress ip0 = IPAddress(192, 168, 43, 75);
 IPAddress *ip[] = {&ip0, &ip1, &ip2};
 
-const char *ssid[] = {"Oneplus 6", "AndroidAP", "TP-Link_DC90"};
-const char *password[] = {"Lacika007", "Nagyponty21", "53745309"};
+const char *ssid[] = {"Oneplus 6", "AndroidAP", "TP-Link_DC90", "KossuthNet"};
+const char *password[] = {"Lacika007", "Nagyponty21", "53745309", "InterNyet17"};
 
-void connectWifi(int which)
+void connectWifi()
 {
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname("Csonak_ESP");
+  WiFiClass::mode(WIFI_STA);
+  WiFiClass::setHostname("Csonak_ESP");
+
+  WiFi.setAutoConnect(true);
   WiFi.begin(ssid[which], password[which]);
   Serial.println("Connecting...");
-  while (WiFi.status() != WL_CONNECTED)
+  while (WiFiClass::status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(1000);
@@ -22,5 +24,6 @@ void connectWifi(int which)
   //long rssi = WiFi.RSSI();
   Serial.print("IP: ");
   Serial.println(WiFi.localIP().toString());
-  Serial.println("Csatlakozva!");
+  Serial.print("Csatlakozva: ");
+  Serial.println(ssid[which]);
 }
